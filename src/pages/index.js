967665544095277c59415256948e99727f795555
Alvin Huang <alvinhuang571@gmail.com>
2019-07-20 16:36:27 -0400

    this is new

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,38 @@
 import React from "react";
+import {Link} from 'gatsby';
+import './mod.css';
+
 
 
 const Index = () => {
   return(
-    <React.Fragment>
-      
-      <center>
-      
-      <h1>Welcome to Video Slack!</h1>
+    
+    <React.Fragment >
+  <div class="wrapper">
+      <center>  
+        <h1>Welcome to Video Slack!</h1>
       <p>Watercoolering for the 21st century workspace!</p>
+</center>
 
+      <Link  to="/">Home</Link><br/>
+            <Link to="/videos">Videos</Link>
+            <div class="logintext">
       <form>
-        Team: <input type="text" placeholder="Team Name"/> <br/>
-       User: <input type="text" placeholder="User Name"/> <br/>
-        Password: <input type="password" placeholder="Password"/> <br/>
+        <b>Team: </b>
+        <input for="uname" type="text" placeholder="Team Name"/> <br/>
+        <b>User: </b>
+       <input type="text" placeholder="User Name"/> <br/>
+       <b>Password: </b>
+        <input type="password" placeholder="Password"/> <br/>
+       
         <input type="submit"/>
       </form>
 
-      </center>
+      </div>
 
+
+      
+      </div>
     </React.Fragment>
   )
 }
--- src/pages/videos.js
+++ src/pages/videos.js
@@ -5,9 +5,22 @@ import {Link} from 'gatsby';
 const Videos = () => {
     return(
         <React.Fragment>
+<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li class="dropdown">
+    <a href="javascript:void(0)" class="dropbtn">League Dropdown</a>
+    <div class="dropdown-content">
+      <a href="#">Little League</a>
+      <a href="#">NCAA</a>
+      <a href="#">Minor League </a>
+    </div>
+  </li>
+</ul>
             <p>Hello from videos</p>
             <Link to='/'>Home</Link><br/>
             <Link to='/recording'>Record Video</Link>
+
         </React.Fragment>
         
     )

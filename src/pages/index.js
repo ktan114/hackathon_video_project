eaddde398dc628644968cb289e6cd65dc8fca9a9
Alvin Huang <alvinhuang571@gmail.com>
2019-07-20 14:29:14 -0400

    modify login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,24 @@
 import React from "react";
 
+
 const Index = () => {
   return(
     <React.Fragment>
+      
+      <center>
+      
       <h1>Welcome to Video Slack!</h1>
       <p>Watercoolering for the 21st century workspace!</p>
 
       <form>
-        Team: <input type="text" placeholder="Team Name"/>
-        User: <input type="text" placeholder="User Name"/>
-        Password: <input type="password" placeholder="Password"/>
+        Team: <input type="text" placeholder="Team Name"/> <br/>
+       User: <input type="text" placeholder="User Name"/> <br/>
+        Password: <input type="password" placeholder="Password"/> <br/>
         <input type="submit"/>
       </form>
+
+      </center>
+
     </React.Fragment>
   )
 }
